aea57b124e2c657132e308fff88bc2f38a6065d3
lizsalazar21 <lizsalazar2105@gmail.com>
2025-10-26 20:53:06 -0600

    ajustes

--- sql/checks.sql
+++ sql/checks.sql
@@ -1,6 +1,6 @@
 CREATE SCHEMA IF NOT EXISTS olist_meta;
 
--- 1) Claves obligatorias no nulas
+-- Claves obligatorias no nulas
 CREATE OR REPLACE VIEW olist_meta.qc_null_keys AS
 SELECT 'olist_fmt.sales' AS table_name, COUNT(*) AS null_keys
 FROM olist_fmt.sales WHERE order_id IS NULL
@@ -10,7 +10,7 @@ UNION ALL
 SELECT 'olist_fmt.customer_satisfaction', COUNT(*) FROM olist_fmt.customer_satisfaction WHERE order_id IS NULL;
 
 
--- 2) Montos no negativos
+-- Montos no negativos
 CREATE OR REPLACE VIEW olist_meta.qc_negative_amounts AS
 SELECT 'olist_fmt.sales' AS table_name, COUNT(*) AS invalid_rows
 FROM olist_fmt.sales WHERE price < 0 OR freight_value < 0 OR order_total_value < 0
@@ -18,13 +18,13 @@ UNION ALL
 SELECT 'olist_fmt.categories', COUNT(*)
 FROM olist_fmt.categories WHERE category_gmv < 0 OR category_freight < 0;
 
--- 3) Reglas de negocio: review_after_delivery_hours >= 0
+-- Reglas de negocio: review_after_delivery_hours >= 0
 CREATE OR REPLACE VIEW olist_meta.qc_review_after_delivery AS
 SELECT COUNT(*) AS invalid_reviews
 FROM olist_fmt.customer_satisfaction
 WHERE review_after_delivery_hours < 0;
 
--- 4) Muestra
+-- Muestra
 SELECT * FROM olist_meta.qc_null_keys;
--- SELECT * FROM olist_meta.qc_negative_amounts;
--- SELECT * FROM olist_meta.qc_review_after_delivery;
+SELECT * FROM olist_meta.qc_negative_amounts;
+SELECT * FROM olist_meta.qc_review_after_delivery;
